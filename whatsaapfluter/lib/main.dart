36177ec0f793e5581c0f31bb916184dcf7126304
Your Name <jrmartins1000@gmail.com>
2019-10-17 19:09:40 -0300

    App Flutter

---
+++ whatsaapfluter/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'Home.dart';
+
+void main(){
+
+  Firestore.instance
+  .collection("usuarios")
+  .document("004")
+  .setData({"nome" : "iPHONE"});
+
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner:  false,
+
+  ));
+
+
+}
+
